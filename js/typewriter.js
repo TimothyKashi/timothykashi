02a979a8183dd6e9782855eca33c117f4c1ec8f9
Timothy Kashi <timothyk5@yahoo.com>
2020-06-25 21:48:26 -0700

    Arrow
    added arrow at bottom of homepage which brings users to 'about' section

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,3 +1,30 @@
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+  console.log('Taking a break...');
+  await sleep(2000);
+  console.log('Two seconds later, showing sleep in a loop...');
+
+  // Sleep in loop
+  for (let i = 0; i < 5; i++) {
+    if (i === 3)
+      await sleep(2000);
+    console.log(i);
+  }
+}
+
+function placeArrow() {
+    var div = document.getElementById('arrow_placer');
+    var arrow = document.createElement("i");
+    arrow.classList.add("fa");
+    arrow.classList.add("fa-chevron-down");
+    arrow.classList.add("bounce-top");
+    div.appendChild(arrow);
+}
+
 function setupTypewriter(t) {
     var HTML = t.innerHTML;
 
@@ -7,7 +34,7 @@ function setupTypewriter(t) {
         tag = "",
         writingTag = false,
         tagOpen = false,
-        typeSpeed = 0.5,
+        typeSpeed = 1,
     tempTypeSpeed = 0;
 
     var type = function() {
@@ -68,3 +95,5 @@ var typer = document.getElementById('typewriter');
 typewriter = setupTypewriter(typer);
 
 typewriter.type();
+
+setTimeout(placeArrow, 9500);
